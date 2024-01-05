be6404570e76c0efe83b23c52d84fe4ded064adc
Mahesh-Kan <maheshwark@gmail.com>
2024-01-04 22:46:22 -0800

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildApp(){
+ echo " building the application"
+ echo "name of file=${MK_NAME}"
+}
+
+def testApp(){
+ echo " testing the application"
+}
+
+def deployApp(){
+   echo " deploying the application" 
+ echo "name of version=${params.version}"
+}
+  
